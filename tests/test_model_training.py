067155ea61f5dece217105326f148d5d657039c1
Kwame Mintah <kwame_mintah@hotmail.co.uk>
2024-03-07 21:10:15 +0000

    test(model_training): add unit test for `start_sagemaker_training_job` function
    mock sagemaker library using `Mock()` and return expected values for each function.

--- tests/test_model_training.py
+++ tests/test_model_training.py
@@ -1,12 +1,19 @@
 import io
+from unittest.mock import Mock
 
 import botocore
+import sagemaker
 from botocore.stub import Stubber, ANY
+from sagemaker import Session
 
-from model_training import lambda_handler, upload_to_output_bucket
 from example_responses import (
     example_get_put_object,
     example_s3_event,
+)
+from model_training import (
+    lambda_handler,
+    upload_to_output_bucket,
+    start_sagemaker_training_job,
 )
 
 
@@ -27,3 +34,39 @@ def test_upload_to_output_bucket():
         assert (
             upload_to_output_bucket(file_obj, "LOCAL_TEST_FILENAME", s3_client) is None
         )
+
+
+def test_start_sagemaker_training_job():
+    # Mock Sagemaker library
+    sagemaker.image_uris.retrieve = Mock()
+    sagemaker.image_uris.retrieve.return_value = (
+        "123456789012.dkr.ecr.eu-west-2.amazonaws.com/xgboost:latest"
+    )
+
+    sagemaker.inputs.TrainingInput = Mock()
+    sagemaker.inputs.TrainingInput.return_value = None
+
+    sagemaker.Session = Mock()
+    sagemaker.Session.return_value = {
+        "boto_session": Session,
+        "boto_region_name": "eu-west-2",
+    }
+
+    # Mock  Estimator object
+    Estimator = Mock()
+
+    sagemaker.estimator.Estimator = Mock()
+    sagemaker.estimator.Estimator.return_value = Estimator
+
+    sagemaker.estimator.Estimator.set_hyperparameters = Mock()
+    sagemaker.estimator.Estimator.set_hyperparameters().return_value = None
+
+    sagemaker.estimator.Estimator.fit = Mock()
+    sagemaker.estimator.Estimator.fit.return_value = None
+
+    assert (
+        start_sagemaker_training_job(
+            region="eu-west-2", framework="xgboost", version="latest"
+        )
+        is None
+    )
